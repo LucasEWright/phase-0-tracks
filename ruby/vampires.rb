65124dbbd7dca85cf595254402362a753a12107c
Lucas Wright <lucasewright@gmail.com>
2016-05-04 12:47:18 -0500

    Added initial questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+def questions
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp
+	puts "Where were you born?"
+	hometown = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	gbread = gets.chomp
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp
+	end
+
+questions
