241a78616458bb74e6b9e02cb2e7a56754190966
Lucas Wright <lucasewright@gmail.com>
2016-05-04 14:52:20 -0500

    Added conditional logic for correct age and yes to garlic bread or insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,12 +3,23 @@ def questions
 	name = gets.chomp
 	puts "How old are you?"
 	age = gets.chomp
-	puts "Where were you born?"
-	hometown = gets.chomp
+	age= age.to_i
+	puts "What year were you born?"
+	birthyear = gets.chomp
+	birthyear = birthyear.to_i
 	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 	gbread = gets.chomp
 	puts "Would you like to enroll in the company’s health insurance?"
 	insurance = gets.chomp
+
+age_check = 2016 - birthyear 
+
+	if age == age_check && (gbread == "yes" || insurance == "yes")
+	print "probably not a vampire"
+	else
+	print "probably a vampire"
 	end
+end
+
 
 questions
