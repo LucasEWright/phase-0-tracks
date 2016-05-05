5a5e290a6ff79704b22d9a8b122bcc6373ec248f
Lucas Wright <lucasewright@gmail.com>
2016-05-04 22:13:31 -0500

    added user input to determine number of loops. Added loopuntil user input is satisfied

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,7 @@
+	puts "How many will be processed?"
+	number = gets.chomp
+	number = number.to_i
+	
 def questions
 	puts "What is your name?"
 	name = gets.chomp
@@ -15,19 +19,23 @@ def questions
 age_check = 2016 - birthyear 
 
 	if age == age_check && (gbread == "yes" || insurance == "yes")
-		print "probably not a vampire"
+	print "probably not a vampire"
 	elsif age != age_check && (gbread == "no" || insurance == "no")
-		print "probably a vampire"
+	print "probably a vampire"
 	elsif (age != age_check && gbread != "yes" && insurance != "yes")
-		print "almost certainly a vampire"
-	elsif name == "Drake Cula" || "Tu Fang"
-		print "definitely a vampire"
+	print "almost certainly a vampire"
+	elsif (name == "Drake Cula" || "Tu Fang" && (age == age_check && gbread == "yes" && insurance == "yes"))
+	print "definitely a vampire"
 	else
 		print "Results inconclusive."
 	end
 	
-
 end
+
+	until number <=1 
+  	questions
+	 number -= 1
+	end
 
 
 questions
