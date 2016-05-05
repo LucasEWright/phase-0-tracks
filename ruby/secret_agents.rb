786fd564653e6b5ac638b16f23395c8b0824b49f
Lucas Wright <lucasewright@gmail.com>
2016-05-05 02:00:26 -0500

    added driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -7,3 +7,11 @@ def decrypt(str)
 str.tr('bcdefghijklmnopqrstuvwxyza', 'abcdefghijklmnopqrstuvwxyz')
 end
 
+encrypt("abc") 
+encrypt("zed") 
+decrypt("bcd") 
+decrypt("afe") 
+
+# this produces the original password because it first calls to encrypt it
+# then to decrypt, flow is controled by the nesting.
+decrypt(encrypt("swordfish"))
