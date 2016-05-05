11738b333b886801509897b344d9b6949a5ae390
Lucas Wright <lucasewright@gmail.com>
2016-05-05 02:27:15 -0500

    corrected psuedocode, added driver code, corrected syntax errors and modified user interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,11 +1,32 @@
+# Psueddcode
+#- determine user input method
+#- identify if the string needs to be converted (array, integer,..)
+#-define method to replace original letter in control sting with letter in modified string
+#-create user interface with needed operators
+#-print results
 
+#replaces letters in user input string with following letter in alphabet
 def encrypt(str)
-str.tr('abcdefghijklmnopqrstuvwxyz', 'bcdefghijklmnopqrstuvwxyza')
+	str.tr('abcdefghijklmnopqrstuvwxyz', 'bcdefghijklmnopqrstuvwxyza')
 end
 
+#replaces letters in user input string with previous letter in alphabet
 def decrypt(str)
-str.tr('bcdefghijklmnopqrstuvwxyza', 'abcdefghijklmnopqrstuvwxyz')
+	str.tr('bcdefghijklmnopqrstuvwxyza', 'abcdefghijklmnopqrstuvwxyz')
 end
+#asks user thier intent
+	puts "Would like to encrypt or decrypt a password"
+		method = gets.chomp
+#asks user for password to modify
+	puts "What is the password?"
+		password = gets.chomp
+#runs method based on user input and prints result
+	if method == "encrypt"
+		encrypt(password)
+	else
+		decrypt(password)
+	end
+
 
 encrypt("abc") 
 encrypt("zed") 
