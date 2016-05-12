28ac40775d6eef0dee05d9b19e908e33a2f60362
Lucas Wright <lucasewright@gmail.com>
2016-05-12 10:58:45 -0500

    created garage nested data structure with 3 ports

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,47 @@
+garage = {
+	port_1: {
+		cars: [
+			fun_car => "Mustang"
+			date_car => "BMW"
+
+		]
+		bikes: [
+			off_road => "mountain"
+			road => "fixie"
+		]
+	}
+	port_2: {
+		cars: [
+			fun_car => "Challenger"
+			date_car => "Jaguar"
+
+		]
+		bikes: [
+			off_road => "ATV"
+			road => "motorcycle"
+		]
+	}
+	port_3: {
+		cars: [
+			fun_car => "350z"
+			date_car => "Mercedes Benz"
+
+		]
+		bikes: [
+			off_road => "dirt bike"
+			road => "scooter"
+		]
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
