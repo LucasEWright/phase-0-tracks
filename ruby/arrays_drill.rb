854aa546dc49470a865acb4eada6fb161088ae0b
Lucas Wright <lucasewright@gmail.com>
2016-05-12 12:53:03 -0500

    renamed test_array to test_array_3, added test_array & test_array_2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,4 +1,15 @@
-def test_array(arr, value)
+def test_array(a, b, c)
+ 	return a, b, c 
+end
+
+
+def test_array_2(a, b)
+ 	return a, b
+end
+
+
+
+def test_array_3(arr, value)
   arr[1].to_i == value.to_i
 end
 
@@ -6,9 +17,6 @@ end
 
 
 
-def test_array_2([a, b, c, d], blue)
- 	 != 100 
-end
 
 
 
@@ -16,29 +24,9 @@ end
 
 
 
+p test_array(1,5,"bunny")
+
+p test_array_2([1,2,3,"x","y","z"],"bunny")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-p test_array([10, 9, 8], 9)
+p test_array_3([10, 9, 8], 9)
