0477f9189a36d51a95e826af8f77edc7141c847f
Lucas Wright <lucasewright@gmail.com>
2016-05-12 13:15:07 -0500

    added release 1 material, previously deleted

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -8,22 +8,45 @@ def test_array_2(a, b)
 end
 
 
-
 def test_array_3(arr, value)
   arr[1].to_i == value.to_i
 end
+#_______________________________________
+
+food = [ ]
+p food 
+
+food.push [
+	"pizza",
+	"taco",
+	"hot dog",
+	"salsa",
+	"chip"
+]
+p food
+
+food.delete_at(2)
+p food
+
+food.insert(2, "chili")
+p food
+
+food.shift
+p food
+
+food.include?("pizza")
+p food
+
+drinks = ["water", "beer", "pop"]
+p drinks
+
+new_array = food | drinks
+p new_array
 
 
 
 
-
-
-
-
-
-
-
-
+#_______________________________________
 p test_array(1,5,"bunny")
 
 p test_array_2([1,2,3,"x","y","z"],"bunny")
