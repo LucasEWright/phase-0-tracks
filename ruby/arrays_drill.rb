fc41a49bc34be931ca7fdc86f038b7598597bfa6
Lucas Wright <lucasewright@gmail.com>
2016-05-12 12:41:18 -0500

    added test_array with boolean to determine eqaul integer values

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,44 @@
+def test_array(arr, value)
+  arr[1].to_i == value.to_i
+end
+
+
+
+
+
+def test_array_2([a, b, c, d], blue)
+ 	 != 100 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+p test_array([10, 9, 8], 9)
