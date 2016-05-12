b6813bd4e33db83e25077b86dc83b733ba135306
Lucas Wright <lucasewright@gmail.com>
2016-05-12 11:36:13 -0500

    restructured data and tested accessing values

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,42 +1,42 @@
 garage = {
 	port_1: {
-		cars: [
-			fun_car => "Mustang"
-			date_car => "BMW"
-
-		]
+		cars: {
+		fun_car: 'mustang',
+		date_car: 'bmw'
+		},
 		bikes: [
-			off_road => "mountain"
-			road => "fixie"
+			"mountain",
+			"fixie"
 		]
-	}
+		},
 	port_2: {
-		cars: [
-			fun_car => "Challenger"
-			date_car => "Jaguar"
-
-		]
+		cars: {
+		fun_car: 'challenger',
+		date_car: 'jaguar'
+		},
 		bikes: [
-			off_road => "ATV"
-			road => "motorcycle"
+			"ATV",
+			"motorcycle"
 		]
-	}
+	},
 	port_3: {
-		cars: [
-			fun_car => "350z"
-			date_car => "Mercedes Benz"
-
-		]
+		cars: {
+		fun_car: 'corvette',
+		date_car: []
+		},
 		bikes: [
-			off_road => "dirt bike"
-			road => "scooter"
+			"dirt bike",
+			"scooter"
 		]
+	},
+}
 
-	}
-
-
-
-
+garage[:port_3][:bikes][0]
+garage[:port_1][:cars]
+garage[:port_1][:cars][:fun_car]
+garage[:port_3][:cars][:date_car].push("benz") 
+garage[:port_3][:cars][:date_car]
+garage[:port_2][:bikes].reverse
 
 
 
