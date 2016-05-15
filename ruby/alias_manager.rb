9a20787994688ab269d47acc2b71f1e173b88e73
Lucas Wright <lucasewright@gmail.com>
2016-05-15 16:43:43 -0500

    added pseudocode and while loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,3 +1,17 @@
+	#prompt user for name to encrypt
+	#covert input to array and reverse
+	#replace vowels with next in vowel sequence
+	#replace consonats with next letters in alphabet (excluding vowels)
+	# => use .tr
+	#print new name
+	#store new name as hash
+	# => will need to access original input as well as alias
+	#repeat process until user input is equal to quit
+	#print statement with both original name and alias for all inputs
+
+
+
+
 	def next_letter ()
 		().tr('abcdefghijklmnopqrstuvwxyz', 'ecdfighjoklmnpuqrstvawxyzb')
 	end
@@ -14,10 +28,17 @@
 def program
 puts "Please enter name:"
 	letters = gets.chomp.downcase
+	
 
 letters = letters.split(' ').reverse
 letters.map! do |letter| 
 	letter.tr('abcdefghijklmnopqrstuvwxyz', 'ecdfighjoklmnpuqrstvawxyzb')
 	end
 end
-	## need to fix downcase issue and add in quit option
+
+program
+
+while program != "quit" do
+	program
+end
+
