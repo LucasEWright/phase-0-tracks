47e6a8c24837dd37d57bf52521136a872ea17410
Lucas Wright <lucasewright@gmail.com>
2016-05-20 16:20:07 -0500

    added four methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,22 @@ class Puppy
     toy
   end
 
+  def speak(times)
+  	puts "Woof!" * times
+  end
+  def roll_over
+  	puts "*rolls over*"
+  end
+  def dog_years(years)
+
+  	
+  end
 end
 
+
+puppy = Puppy.new
+puppy.fetch("boyfriend")
+puppy.speak(5)
+
+
+#_____________________
