1eb765d23fbdf0b508eedb171f209d0b13ec20c4
Lucas Wright <lucasewright@gmail.com>
2016-05-19 22:49:08 -0500

    added three attribute changing methods and two getter methods for age and ethnicity

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,4 @@
-
-class Puppy
+class Santas
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -8,11 +7,52 @@ class Puppy
     puts "That was a good #{cookie}!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
+     "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
+  
+  def celebrate_birthday
+  	@age = age + 1
+  	puts "Now they are #{age}!"
+  end 
+
+  def get_mad_at(mad_at)
+  	@reindeer_ranking = @reindeer_ranking.push(mad_at)
+  	puts @reindeer_ranking
+  end
+  
+  def age
+  	@age
+  end
+
+  def ethnicity
+  	@ethnicity
+  end
+
 end
 
-puppy = Puppy.new
-puppy.eat_milk_and_cookies("PB")
-puppy.speak
+gender = ["agender", "female", "male", "bigender"]
+ethnicity = ["white", "black", "native american", "latino"]
+santas = []
+
+puts "creating some diversity..."
+gender.each do |gender|
+	puts "We now have a #{gender} Santa. Welcome!"
+	puts "___________"
+end
+ethnicity.each do |ethnicity|
+	puts "We now have a #{ethnicity} Santa. Welcome!"
+	puts "___________"
+end
+
+
+santas = Santas.new("male", "white")
+santas.eat_milk_and_cookies("PB")
+santas.speak
+santas.get_mad_at("Lucas")
+santas.celebrate_birthday
