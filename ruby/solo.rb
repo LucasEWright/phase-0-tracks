b0a3dde9c0f768d64515508878a72990173399a8
Lucas Wright <lucasewright@gmail.com>
2016-05-21 13:55:50 -0500

    added pseudocode to build transformer and designed a corresponding class

---
+++ ruby/solo.rb
@@ -0,0 +1,9 @@
+#Build a transfromer#
+#select three atributes: (age, height(number), temperment(str) )
+#
+#select three methods: 
+#(name (first, last))- says hello, 
+#transforms_into(whatever(str)), 
+#destroy -prints str)   
+#
+#
