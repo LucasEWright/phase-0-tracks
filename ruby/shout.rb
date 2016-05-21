ac0a285c3b4d7592cd1460ced31a1eea808dab18
Lucas Wright <lucasewright@gmail.com>
2016-05-21 16:31:30 -0500

    added module shout with two methods to Shout.rb and added module to flight.rb per Release 2

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,36 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#    def self.yelling_happily(words)
+#    words + "!!!" + " :)"
+#  end
+#end
+
+#Shout.yelling_happily("Lucas")
+#Shout.yell_angrily("Ivan")
+
 module Shout
-  def self.yell_angrily(words)
+    def yell_angrily(words)
     words + "!!!" + " :("
   end
-    def self.yelling_happily(words)
+    def yelling_happily(words)
     words + "!!!" + " :)"
   end
 end
 
-Shout.yelling_happily("Lucas")
-Shout.yell_angrily("Ivan")
+
+class Boyfriend
+	include Shout
+end
+
+class Ex_Boyfriend
+	include Shout
+end
+
+boyfriend = Boyfriend.new
+bird.take_off("Ivan")
+
+#____Driver Code______
+ex_boyfriend = Ex_Boyfriend.new
+ex_boyfriend.yell_angrily("Dummy")
