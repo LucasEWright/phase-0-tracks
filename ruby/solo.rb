9c3759b0b973a39f25dd6ed583339d993ee502e5
Lucas Wright <lucasewright@gmail.com>
2016-05-21 14:16:26 -0500

    added Transformer class with 3 new attributes and 3 new new methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -18,4 +18,45 @@
 # BEHAVIOR -----------------------
 # transforms per user input
 # destroys everything 
+#===============================================================
+
+class Transformer
+	attr_reader :mood
+	attr_accessor :type, :age
+
+	def initialize(height, mood)
+		puts "Initializing a new transformer"
+		@type = "robot"
+		@age = 0
+		@height = height.to_i
+		@mood = mood
+		puts "who is a #{mood} #{type} that is #{height} feet tall."
+	end	
+
+	def name(first, last)
+		@name = first + last
+		puts "Hello, my name is #{name}"
+	end
+	
+	def transforms_into(anything)
+		@transforms_into = anything
+		puts "I've turned into a #{anything}"
+	end
+
+	def destroy
+		puts "Self-destructing in 3...2...1..."		
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+end
 
