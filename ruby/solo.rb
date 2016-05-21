2f7ba3c041f80a5612ec96001f1583d62e8bdf5a
Lucas Wright <lucasewright@gmail.com>
2016-05-21 14:49:48 -0500

    added user input that loops until user is done

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -27,14 +27,16 @@ class Transformer
 	def initialize(height, mood)
 		puts "Initializing a new transformer"
 		@type = "robot"
-		@age = 0
+		@age = rand(0...100)
 		@height = height.to_i
 		@mood = mood
-		puts "who is a #{mood} #{type} that is #{height} feet tall."
+		puts "who is a #{age} year old #{mood} #{type} that is #{height} feet tall."
 	end	
 
 	def name(first, last)
-		@name = first + last
+		@first = first
+		@last = last
+		@name = @first + @last
 		puts "Hello, my name is #{name}"
 	end
 	
@@ -46,6 +48,35 @@ class Transformer
 	def destroy
 		puts "Self-destructing in 3...2...1..."		
 	end
+end
+
+
+def user_loop 
+index = 1
+while index > 0
+
+puts "How tall, in feet, is your Transformer?"
+	@height = gets.chomp
+puts "What is thier temperment?"
+	@mood = gets.chomp
+puts "What is thier first name?"
+	@first = gets.chomp
+puts "What is thier last name?"
+	@last = gets.chomp
+puts "What does your Transformer turn into?"
+	@transforms_into = gets.chomp
+puts "Would you like to create another Transformer? yes or no"
+	answer = gets.chomp
+	if answer == "yes" 
+		user_loop
+	else 
+		puts "Thanks for playing!"
+		index -=1
+	end
+end 
+end
+
+user_loop
 
 
 
