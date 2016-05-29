556af01d7951855b2ba10498e659f6a68c0c5b4b
Lucas Wright <lucasewright@gmail.com>
2016-05-29 17:54:47 -0500

    added colors and names arrays, and code to convert arrays into a key/value object

---
+++ js/data_structures.js
@@ -0,0 +1,22 @@
+var colors = ["blue", "red", "white", "gold"];
+var names = ["Lucas", "Ivan", "Frida", "Bella"];
+
+colors.push("pink")
+names.push("Hiedi")
+
+var horseObject = {},
+    i,
+    keys =  names //['one', 'two', 'three'],
+    values = colors  //['a', 'b', 'c'];
+
+for (i = 0; i < keys.length; i++) {
+    horseObject[keys[i]] = values[i];
+    horseObject 
+}
+
+
+//for (var i = 0; i < colors.length; i++) {
+//	console.log(colors[i]);
+//}
+
+//console.log("Like my coat, my horses have many colors " + names['name'] + "-" + colors['colors']);
