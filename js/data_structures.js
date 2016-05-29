28416542ea7dc9e592fe1dfd391d5a358673df43
Lucas Wright <lucasewright@gmail.com>
2016-05-29 18:20:41 -0500

    added new contructor function to create car

--- js/data_structures.js
+++ js/data_structures.js
@@ -20,3 +20,33 @@ for (i = 0; i < keys.length; i++) {
 //}
 
 //console.log("Like my coat, my horses have many colors " + names['name'] + "-" + colors['colors']);
+
+//============CAR=======================
+
+var car = {model: 'Spot', year: 3, topSpeed: "100mph"};
+
+
+function Car(model, year, topSpeed) {
+	console.log("Our new car:", this);
+	this.go = function() { console.log("Vrrrm!"); };
+	
+	console.log("CAR INITIALIZATION COMPLETE");
+}
+console.log("Let's build a new car ...");
+// Here is where we call that constructor function
+// that we just made ...
+var anotherCar = new Car("Mustang", 2016, "145mph");
+console.log(anotherCar);
+console.log("Our new car can go:");
+anotherCar.go();
+console.log("----");
+
+console.log("Let's build another car ...");
+var yetAnotherCar = new Car("280z", 1978, "155mph");
+//not printing out new model, NEED TO FIX
+console.log(yetAnotherCar.model);
+console.log("This new dog can go, too:");
+yetAnotherCar.go();
+console.log("----");
+
+//NEED TO ADD driver code
