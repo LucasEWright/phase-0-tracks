2b6ae53632c171ad73579d3002fe7ed3d3742af5
Lucas Wright <lucasewright@gmail.com>
2016-05-30 12:14:44 -0500

    corrected syntax errors and finalized car function

--- js/data_structures.js
+++ js/data_structures.js
@@ -28,6 +28,10 @@ var car = {model: 'Spot', year: 3, topSpeed: "100mph"};
 
 function Car(model, year, topSpeed) {
 	console.log("Our new car:", this);
+	
+	this.model = model;
+	this.year = year;
+	this.topSpeed = topSpeed;
 	this.go = function() { console.log("Vrrrm!"); };
 	
 	console.log("CAR INITIALIZATION COMPLETE");
@@ -43,8 +47,7 @@ console.log("----");
 
 console.log("Let's build another car ...");
 var yetAnotherCar = new Car("280z", 1978, "155mph");
-//not printing out new model, NEED TO FIX
-console.log(yetAnotherCar.model);
+console.log(yetAnotherCar);
 console.log("This new dog can go, too:");
 yetAnotherCar.go();
 console.log("----");
