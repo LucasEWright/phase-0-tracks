fc944e9097c1b60cb08427638b9a6da0622a7ab0
Lucas Wright <lucasewright@gmail.com>
2016-05-30 22:34:06 -0500

    added compare function that takes two objects and compares for similarties

--- js/algos.js
+++ js/algos.js
@@ -14,8 +14,35 @@ var array = ["long", "longer", "longestest", "longest"];
  
  for(var i=0; i < array.length; i++) {
     if (array[i].length > strLength) {
-        index = i; strLength = array[i];
+        index = i; strLength = array[i].length;
     }
 }
 //return array[i];
 console.log(strLength)
+
+//===========================================================================
+//	function that accepts objects as arguments 
+//	use "or" operator to compare the name and age of each object 
+//  **had to use dot function to call name and age for it to work**
+//	use return method to return true or false
+//
+
+person1 = {name: "Steven", age: 54}
+person2 = {name: "Jane", age: 54}
+
+function compareLong(a, b) {
+	return (a.name === b.name || a.age === b.age);
+}
+
+compareLong(person1, person2);
+
+//	solution I found while looking for a solution to something must less specefic, I assume this is what
+// 	would be considered a "slick search function"
+
+//	function compare(a, b) {
+// 	return JSON.stringify(a, b) === JSON.stringify(a, b);
+//}
+//compare(person1, person2);
+
+//============================================================================
+
