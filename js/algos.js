06364459b9386720941857b58b1743914ba84f08
Lucas Wright <lucasewright@gmail.com>
2016-05-30 19:07:31 -0500

    added pseudocode for longest string algo

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+
+//=======pseudocode for relaeae 1- longest string in array======================
+//	Create an array with three sets of strings of three different char. lengths.
+//	Create a base value index (0) and a variable that will eventually be our longest string.
+//	Loop over each index in the array returning each strings length
+//	Method to check legnth of string, determine if it's greater than the current varible, if it is greater replace, if not move on
+//	Post loop, return holding variable. 
+//==============================================================================
