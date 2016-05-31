58b4315b10224fc7448f3016b35782a191242cdd
Lucas Wright <lucasewright@gmail.com>
2016-05-30 21:21:37 -0500

    added function to return longest string in array

--- js/algos.js
+++ js/algos.js
@@ -6,3 +6,15 @@
 //	Method to check legnth of string, determine if it's greater than the current varible, if it is greater replace, if not move on
 //	Post loop, return holding variable. 
 //==============================================================================
+
+var array = ["long", "longer", "longest"];
+
+ index = 0; 
+ strLength = array[0].length;
+ for(var i=1; i< array.length; i++) {
+    if(array[i].length > strLength) {
+        index = i; strLength = array[i].length;
+    }
+}
+//return array[i];
+//console.log(strLength);
