95fae5d7aed1540048e01a208d58be723459615a
Lucas Wright <lucasewright@gmail.com>
2016-05-31 00:02:21 -0500

    added comments highlighting problems still needing to be corrected

--- js/algos.js
+++ js/algos.js
@@ -1,11 +1,11 @@
 
-//=======pseudocode for relaeae 1- longest string in array======================
+//=======pseudocode for relaeae 0- longest string in array======================
 //	Create an array with three sets of strings of three different char. lengths.
 //	Create a base value index (0) and a variable that will eventually be our longest string.
 //	Loop over each index in the array returning each strings length
 //	Method to check legnth of string, determine if it's greater than the current varible, if it is greater replace, if not move on
 //	Post loop, return holding variable. 
-//==============================================================================
+
 
 var array = ["long", "longer", "longestest", "longest"];
 
@@ -20,11 +20,15 @@ var array = ["long", "longer", "longestest", "longest"];
 //return array[i];
 console.log(strLength)
 
-//===========================================================================
-//	function that accepts objects as arguments 
-//	use "or" operator to compare the name and age of each object 
+// Problems
+// It's returning the length of the longest string, not the actually string
+
+
+//============pseudocode for relaeae 1- key value match===============================
+//	Function that accepts objects as arguments 
+//	Use "or" operator to compare the name and age of each object 
 //  **had to use dot function to call name and age for it to work**
-//	use return method to return true or false
+//	Use return method to return true or false
 //
 
 person1 = {name: "Steven", age: 54}
@@ -40,7 +44,7 @@ compareLong(person1, person2); 	//=> true
 compareLong(person1, person3);	//=> true
 compareLong(person2, person4);	//=> false8
 
-//	solution I found while looking for a solution to something must less specefic, I assume this is what
+//	The following is a solution I found while looking for a solution to something must less specefic, I assume this is what
 // 	would be considered a "slick search function"
 
 //	function compare(a, b) {
@@ -48,7 +52,7 @@ compareLong(person2, person4);	//=> false8
 //}
 //compare(person1, person2);
 
-//============================================================================
+//=============pseudocode for relaeae 2-Random test data================================
 //	create an empty array 
 //	take the user input (number of words)
 //	set number to index
@@ -59,10 +63,10 @@ compareLong(person2, person4);	//=> false8
 //	prints array
 //	call longest word method (not currently working)
 
+
 // 	RANDOM WORD GENERATOR
 //	a is set equal to a random number 
 a = Math.floor(Math.random() * 20);
-
 
 function wordGenerator(a) {
     var text = "";
@@ -74,7 +78,10 @@ function wordGenerator(a) {
 }
 
 wordGenerator(a)
+
 //=== === ==== ==== ==== ====
+
+// 	ARRAY GENERATOR
 // funciton to use the wordGenerator to create a number (b) of workd and shovel them into an array
 function arrayMaker(b) {
 	var array = [];
@@ -82,4 +89,12 @@ function arrayMaker(b) {
 	array.push(wordGenerator(a)*b) 
 }
 arrayMaker(3)
+
+// problems
+// array maker not working 
+
+
+//COMMENTS
+// Still need to figure out array generator and get release 0 to return the string, not the 
+// length of the longest string
 
