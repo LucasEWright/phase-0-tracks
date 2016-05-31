633737c6434d5b95554496ababfdb0fdee450dc8
Lucas Wright <lucasewright@gmail.com>
2016-05-30 23:54:02 -0500

    created word generator function to take the number of words to be created and return them in an error. Not currently working.

--- js/algos.js
+++ js/algos.js
@@ -29,12 +29,16 @@ console.log(strLength)
 
 person1 = {name: "Steven", age: 54}
 person2 = {name: "Jane", age: 54}
+person3 = {name: "Steven", age: 100}
+person4 = {name: "Ivan", age: 35} 
 
 function compareLong(a, b) {
 	return (a.name === b.name || a.age === b.age);
 }
 
-compareLong(person1, person2);
+compareLong(person1, person2); 	//=> true
+compareLong(person1, person3);	//=> true
+compareLong(person2, person4);	//=> false8
 
 //	solution I found while looking for a solution to something must less specefic, I assume this is what
 // 	would be considered a "slick search function"
@@ -45,4 +49,37 @@ compareLong(person1, person2);
 //compare(person1, person2);
 
 //============================================================================
+//	create an empty array 
+//	take the user input (number of words)
+//	set number to index
+//	random word generator using .random of varying type and length
+//	per the index, create random words and shovel them into the array ** random word function will need use the 	
+//	random number generator
+//	create 10 arrays
+//	prints array
+//	call longest word method (not currently working)
+
+// 	RANDOM WORD GENERATOR
+//	a is set equal to a random number 
+a = Math.floor(Math.random() * 20);
+
+
+function wordGenerator(a) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for( var i=0; i < a; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+wordGenerator(a)
+//=== === ==== ==== ==== ====
+// funciton to use the wordGenerator to create a number (b) of workd and shovel them into an array
+function arrayMaker(b) {
+	var array = [];
+//	creating (b) number of words and pushing them into the array
+	array.push(wordGenerator(a)*b) 
+}
+arrayMaker(3)
 
