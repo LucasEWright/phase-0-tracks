ac1d97ffa013262673c08c752d9aaf098a360bfe
Lucas Wright <lucasewright@gmail.com>
2016-06-05 11:59:01 -0500

    added code to store original and alias names in hash

--- ruby/alias_manager_v2.rb
+++ ruby/alias_manager_v2.rb
@@ -4,6 +4,34 @@ consonant_str = ("abcdefghijklmnopqrstuvwxyz")
 vowel_array = ["a", "e", "i", "o", "u"]
 consonant_array = ("abcdefghijklmnopqrstuvwxyz").split("")
 
+def next_letter(a)
+	a.tr('abcdefghijklmnopqrstuvwxyz', 'ecdfighjoklmnpuqrstvawxyzb')
+end
+
+def next_vowel(a)
+	a.tr('aeiou', 'eioua')
+end
+
+def next_con(a)
+	a.tr('bcdfghjklmnpqrstvwxyz', 'bcdfghjklmnpqrstvwxyz')
+end
+
+new_name = []
+
+
 puts "Please enter a name:"
-real_name = gets.chomp.downcase.split(" ").reverse
-real_name
+first_name = gets.chomp.upcase
+new_name.push(first_name)
+real_name = first_name
+real_name = real_name.downcase.split(" ").reverse
+real_name = real_name.join(" ").downcase.split("")
+
+	real_name.map! { |x|
+	x.tr('abcdefghijklmnopqrstuvwxyz', 'ecdfighjoklmnpuqrstvawxyzb')
+	} 
+	real_name = real_name.join("").upcase
+	new_name.push(real_name)
+	#new_name = new_name.join("")
+newHash = Hash[*new_name]
+p new_name
+p newHash
