2ee08585db580e4dd96a0548c3d6fb618d609dd7
Lucas Wright <lucasewright@gmail.com>
2016-06-05 15:24:23 -0500

    corrected syntax errors of loop and shoveling to hash. Still problem with output capitalization

--- ruby/alias_manager_v2.rb
+++ ruby/alias_manager_v2.rb
@@ -16,22 +16,42 @@ def next_con(a)
 	a.tr('bcdfghjklmnpqrstvwxyz', 'bcdfghjklmnpqrstvwxyz')
 end
 
-new_name = []
+@new_name = []
+@newHash = Hash.new
+@index = 1
 
-
+def test_method
 puts "Please enter a name:"
-first_name = gets.chomp.upcase
-new_name.push(first_name)
-real_name = first_name
-real_name = real_name.downcase.split(" ").reverse
-real_name = real_name.join(" ").downcase.split("")
+@first_name = gets.chomp.upcase
+	if @first_name == "QUIT"
+		p "Stopped"
+		@newHash = Hash[*@new_name]
+		@newHash.each { |name1, name2| p "#{name2} is actually #{name1}." } 
+		@index = 0
+	else
+		
+	@new_name.push(@first_name)
+	real_name = @first_name
+	real_name = real_name.split(" ").reverse
+	real_name = real_name.join(" ").downcase.split("")
 
 	real_name.map! { |x|
 	x.tr('abcdefghijklmnopqrstuvwxyz', 'ecdfighjoklmnpuqrstvawxyzb')
 	} 
 	real_name = real_name.join("").upcase
-	new_name.push(real_name)
-	#new_name = new_name.join("")
-newHash = Hash[*new_name]
-p new_name
-p newHash
+	p "New name is #{real_name}"
+		real_name_2 = real_name
+		@new_name.push(real_name_2)
+	end
+	
+end
+	
+while @index != 0
+	test_method
+end
+
+
+
+
+
+
