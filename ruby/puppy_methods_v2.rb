4767f9353e243e490fef2242a9613e12f42fa7c4
Lucas Wright <lucasewright@gmail.com>
2016-06-08 22:14:09 -0500

    added cat class with a pet and bite method.  Added loop that stores 50 instances of in an array. Added interation to run methods on each instance

--- ruby/puppy_methods_v2.rb
+++ ruby/puppy_methods_v2.rb
@@ -30,3 +30,38 @@ puppy.speak(2)
 puppy.roll_over
 puppy.dog_years(5)
 puppy.play_dead
+
+#===================================
+
+class Cats
+  
+  def initialize
+  end
+
+  def pet
+    puts "This cat wants you to pet them..."
+    
+  end
+
+  def bite
+    puts "...and now they want to bite you."
+  end
+end
+  
+cats_array = []
+loop_num = 0 
+
+while loop_num < 50
+  cats_array.push(Cats.new)
+  loop_num += 1
+end
+
+cats_array.each do |pet|
+  pet.pet
+  pet.bite
+end
+
+#Driver code for class cats 
+cats = Cats.new
+cats.pet
+cats.bite
