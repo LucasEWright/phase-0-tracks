2d66291c72ad233286e0ee3ff210cd13ffdb699d
Lucas Wright <lucasewright@gmail.com>
2016-06-10 14:54:04 -0500

    added code to pass all tests.

---
+++ ruby/list/todo_list_v2.rb
@@ -0,0 +1,19 @@
+class TodoList
+
+	def initialize(*args)
+		@list = *args	
+	end
+
+	def get_items
+		@list[0]
+	end
+	def add_item(item)
+		@list[0] << item
+	end
+	def delete_item(item)
+		@list[0].delete(item) 
+	end
+	def get_item(index)
+		@list[0][index]
+	end
+end	
