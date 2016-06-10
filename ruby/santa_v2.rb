97a6919cc039fd84105e2c212431b887e190efda
Lucas Wright <lucasewright@gmail.com>
2016-06-10 10:06:24 -0500

    modified initialize method to pass gender and ethnicity, and set reindeer rankings and default age of 0

--- ruby/santa_v2.rb
+++ ruby/santa_v2.rb
@@ -1,6 +1,10 @@
 class Santa
 
-	def intialize
+	def intialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		age = 0
 		p "intializing Santa instance..."
 	end
 
