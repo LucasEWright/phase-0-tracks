96b07e9ab06e1933537d848b3437920810da5aef
Lucas Wright <lucasewright@gmail.com>
2016-06-10 11:23:24 -0500

    added while loop to build random array of santas.

--- ruby/santa_v2.rb
+++ ruby/santa_v2.rb
@@ -1,6 +1,9 @@
 class Santa
 
-	def intialize(gender, ethnicity)
+attr_reader :ethnicity
+attr_accessor :gender, :age
+
+	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
 		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
@@ -16,9 +19,41 @@ class Santa
 		p "That was a good #{cookie}"
 	end
 
-
-
-
-
-
 end 
+
+new_santas =[]
+index = 0
+
+ethnicity_array = ["white", "black", "latino", "asian", "alien"]
+gender_array = ["male", "female", "agender", "non-binary"]
+
+#====== driver code ======
+
+
+
+
+#new array to hold generated santas
+santas =[]
+#posible gender array
+gender = ["male", "female", "agender", "non-binary"]
+#possbile ethnicity array
+ethnicity = ["white", "black", "latino", "asian", "alien"]
+#number of santas to create
+index = 3
+
+while index > 0
+  new_santas = Santa.new(gender.sample, ethnicity.sample)
+  new_santas.age = rand(1..140)
+  santas.push(new_santas)
+  index -= 1
+  end
+
+p santas
+#_________________________
+
+#_Driver code___
+#santas = Santas.new("male", "white")
+#santas.eat_milk_and_cookies("PB")
+#santas.speak
+#santas.get_mad_at("Lucas")
+#santas.celebrate_birthday
