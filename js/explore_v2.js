03018ce0728048350a7a0235785819bd6d2abebe
Lucas Wright <lucasewright@gmail.com>
2016-06-18 11:10:30 -0500

    added pseudocode for reverse method

---
+++ js/explore_v2.js
@@ -0,0 +1,6 @@
+// create a method that takes a parameter(string)
+// turn string into individual characters that can be manipulated 
+// reverse the order of the characters
+// turn back into a string
+// display new string
+//
