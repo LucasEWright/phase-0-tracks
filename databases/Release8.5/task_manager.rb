6d33b4f2c7fad3835d7cc259b24908e6a3e95353
Lucas Wright <lucasewright@gmail.com>
2016-06-24 14:45:16 -0500

    created task manager with linked database. Added methods to add and complete tasks

---
+++ databases/Release8.5/task_manager.rb
@@ -0,0 +1,55 @@
+# gems
+require 'sqlite3'
+# create database
+db = SQLite3::Database.new("tasks.db")
+db.results_as_hash = true
+
+#create_tbl = <<-SQL
+#CREATE TABLE IF NOT EXISTS task_list (
+#		id INTEGER PRIMARY KEY,
+#		Task VARCHAR(255),
+#		Priority VARCHAR(255),
+#		Type VARCHAR(255),
+#		DueDate DATE,
+#		Complete BOOLEAN
+#	)
+#	SQL 
+
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS task_list (
+		id INTEGER PRIMARY KEY,
+		Task VARCHAR(255),
+		Priority VARCHAR(255),
+		Type VARCHAR(255),
+		DueDate DATE,
+		Complete BOOLEAN
+	)
+	SQL
+
+#create task list table
+db.execute(create_table_cmd)
+
+def new_task(db, task, priority, type, due)
+	db.execute("INSERT INTO task_list (Task, Priority, Type, DueDate)
+		VALUES (?, ?, ?, ?)", [task, priority, type, due])
+end
+
+#new_task(db, "Buy bread", "low", "personal", "7/4/2018")
+
+def complete_task(db, task_id)
+	db.execute("DELETE FROM task_list WHERE id = #{task_id}")
+end
+#complete_task(db, 2)
+
+# => not currently working, returns error that a column name user-input new_priority doesn't exist
+# => WILL COME BACK 
+#def change_priority(db, task_id, new_priority)
+#	db.execute("UPDATE task_list SET Priority = #{new_priority} WHERE id = #{task_id}")
+#end
+#change_priority(db, 1, 'high')
+
+def change_date(db) #(db, task_id, new_date)
+	#db.execute("UPDATE task_list SET DueDate = #{new_date} WHERE id = #{task_id}")
+	db.execute("UPDATE task_list SET DueDate = #{'new_date'} WHERE id = #{task_id}")
+end
+change_date(db, 1, 12/1/2017)
