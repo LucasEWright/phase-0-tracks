9e7753bf0665d6657e83d4debdde09835fb907fd
Lucas Wright <lucasewright@gmail.com>
2016-07-07 12:49:03 -0500

    added js to change background color to pink when loaded.

--- web_dev/client_side_js/javascript.js
+++ web_dev/client_side_js/javascript.js
@@ -1,10 +1,14 @@
 console.log("The script is running!!!")
 
+//=========================
+
+document.body.style.backgroundColor = "pink"
 
 function addPurpleBorder() {
 	var photo = document.getElementById("puppy")
-	photo.style.border = "10px solid purple"
+	photo.style.border = "10px solid purple";
 	}
 
 var photo = document.getElementById("puppy")
 photo.addEventListener("click", addPurpleBorder)
+
